ac00027d9f9f8f6af8932dd814fb19482249634d
Arcyvilk <arcyvilk@pm.me>
2025-07-20 16:34:26 +0200

    refactor: 💡 initial css

---
+++ src/views/MissionControlDialogue/MissionControlDialogue.tsx
@@ -0,0 +1,5 @@
+import s from "./MissionControlDialogue.module.scss"
+
+export const MissionControlDialogue = () => {
+  return <div className={s.container}>Mission Control Dialogue</div>
+}
---
+++ src/views/MissionControlDialogue/index.tsx
@@ -0,0 +1,1 @@
+export { MissionControlDialogue } from "./MissionControlDialogue"
