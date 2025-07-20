7d85d9bdf6f36cc65858fb0510758a95c9a09bd4
Arcyvilk <arcyvilk@pm.me>
2025-07-20 17:48:14 +0200

    feat: 🎸 block playing the game on too small screen

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,23 @@
 import { MissionControlDialogue } from "./components/MissionControlDialogue"
+import { useScreenWidth } from "./utils/useScreenWidth"
 
 import "./App.css"
 
 export const App = () => {
+  const { width } = useScreenWidth()
+  const isMobile = width <= 800
+
+  if (isMobile)
+    return (
+      <div className="warning">
+        This game requires a larger screen to play. Please use a desktop or
+        tablet device.
+      </div>
+    )
+
   return (
-    <>
+    <div className="app">
       <MissionControlDialogue />
-    </>
+    </div>
   )
 }
--- src/components/MissionControlDialogue/MissionControlDialogue.tsx
+++ src/components/MissionControlDialogue/MissionControlDialogue.tsx
@@ -1,3 +1,4 @@
+import { useState } from "react"
 import { dialogue } from "../../data/dialogue"
 import { MissionControlAvatar } from "../MissionControlAvatar"
 import { TypeAnimation } from "react-type-animation"
@@ -5,17 +6,27 @@ import { TypeAnimation } from "react-type-animation"
 import "./MissionControlDialogue.css"
 
 export const MissionControlDialogue = () => {
+  const [isTyping, setIsTyping] = useState(true)
+
   const currentDialogue = dialogue[0].text
 
+  const onClick = () => {
+    setIsTyping(false)
+  }
+
   return (
-    <div className="container">
+    <div className="container" onClick={onClick}>
       <div className="mcAvatar">
         <MissionControlAvatar />
       </div>
 
       <div className="title">Mission Control</div>
       <div className="description">
-        <TypeAnimation sequence={[currentDialogue]} speed={66} />
+        {isTyping ? (
+          <TypeAnimation sequence={[currentDialogue]} speed={66} />
+        ) : (
+          currentDialogue
+        )}
       </div>
     </div>
   )
