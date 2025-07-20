1e1e3ee46690c79400f784d8baacbeb4118bb66b
Arcyvilk <arcyvilk@pm.me>
2025-07-20 17:27:54 +0200

    refactor: 💡 add static dialogue

--- src/components/MissionControlDialogue/MissionControlDialogue.tsx
+++ src/components/MissionControlDialogue/MissionControlDialogue.tsx
@@ -1,11 +1,19 @@
+import { dialogue } from "../../data/dialogue"
 import { MissionControlAvatar } from "../MissionControlAvatar"
+
 import "./MissionControlDialogue.css"
 
 export const MissionControlDialogue = () => {
+  const currentDialogue = dialogue[0].text
+
   return (
     <div className="container">
-      <MissionControlAvatar />
+      <div className="mcAvatar">
+        <MissionControlAvatar />
+      </div>
+
       <div className="title">Mission Control</div>
+      <div className="description">{currentDialogue}</div>
     </div>
   )
 }
